1fe4f72364491fb09731e0bd11282b25141bf45a
Yeowoon <woons.2016@gmail.com>
2018-09-01 14:42:36 +0900

    updated

--- code.R
+++ code.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(gcookbook)
 
 #---------------------------------------------------------
 # Anscombe’s quartet
@@ -28,41 +29,6 @@ c4 <- ggplot(anscombe, aes(x = x4, y = y4)) +
   geom_smooth(method = "lm")
 
 gridExtra::grid.arrange(c1, c2, c3, c4, ncol = 2)
-
-#---------------------------------------------------------
-# 데이터
-#---------------------------------------------------------
-
-# 아래 각각의 데이터셋 구조를 살펴봅시다
-# iris 데이터셋의 변형에 따른 열은 어떻게 달라졌나요?
-str(iris)
-str(iris.wide)
-str(iris.tidy)
-
-
-# Rplot.jpg와 같은 차트를 만드려면 어떤 데이터를 써서 만들어야할까요?
-ggplot(___, aes(x = ___, y = ___, col = ___)) +
-  geom_jitter() +
-  facet_grid(. ~ ___)
-
-# iris.tidy를 만들어보자
-iris.tidy <- iris %>%
-  gather(key, Value, -Species) %>%
-  separate(key, c("Part", "Measure"), "\\.")
-
-# 이번에는 iris.wide를 사용한다
-head(iris)
-head(iris.wide)
-head(iris.tidy)
-
-# 어떤 데이터를 써서 Rplot01처럼 만들 수 있을까?
-ggplot(iris.wide, aes(x = Length, y = Width, color = Part)) +
-  geom_jitter() +
-  facet_grid(. ~ Species)
-
-# 실습 데이터 cafe.csv & judge_b.csv를 gather 해보세요
-df_cafe <- read_csv("data/cafe.csv")
-df_health <- read_csv("data/judge_b.csv")
 
 #---------------------------------------------------------
 # Grammar of Graphics Layer Concept
@@ -110,7 +76,6 @@ ggplot(data = iris,
   coord_cartesian(xlim = c(5, 6)) +
   theme(strip.background = element_rect(colour = "black", fill = "white"))
 
-
 #---------------------------------------------------------
 # base vs. ggplot2 graphics
 #---------------------------------------------------------
@@ -124,3 +89,44 @@ ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) +
   geom_point() +      
   geom_point(aes(x=Petal.Length, y=Petal.Width), color = "red") + 
   labs(x="Length", y="Width")
+
+
+#---------------------------------------------------------
+# 데이터
+#---------------------------------------------------------
+load("iris.RData")
+
+# 아래 각각의 데이터셋 구조를 살펴봅시다
+# iris 데이터셋의 변형에 따른 열은 어떻게 달라졌나요?
+str(iris)
+str(iris.wide)
+str(iris.tidy)
+
+# iris로 시각화(1)
+ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, color = Species)) +
+  geom_point() +
+  facet_grid(. ~ Species) +
+  scale_x_discrete()
+
+# iris.wide로 시각화(2)
+head(iris.wide)
+ggplot(iris.wide, aes(x = Length, y = Width, color = Species)) +
+  geom_point() +
+  facet_grid(. ~ Species)
+
+# iris.tidy로 시각화(3)
+head(iris.tidy)
+ggplot(iris.tidy, aes(x = Species, y = Value, color = Part)) +
+  geom_jitter() +
+  facet_grid(. ~ Measure) 
+
+
+# iris.tidy를 만들어보자
+iris.tidy <- iris %>%
+  gather(key, Value, -Species) %>%
+  separate(key, c("Part", "Measure"), "\\.")
+
+# 실습 데이터 cafe.csv & judge_b.csv를 gather 해보세요
+# 10분 후에 함께 풀이할 예정입니다
+df_cafe <- read_csv("data/cafe.csv")
+
